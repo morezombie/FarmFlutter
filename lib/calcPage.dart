556aea7090c0c88e22a1d53427a28eab0647f518
panghuachong <panghuachong@gmail.com>
2021-04-19 20:03:44 +0800

    Added a drawer by swiping

--- lib/calcPage.dart
+++ lib/calcPage.dart
@@ -97,6 +97,43 @@ class _InitiatorState extends State<Initiator> {
         },
         child: Text("Go!"),
       ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: DecorationImage(
+                  image: AssetImage("lib/assets/bb.jpg"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            ListTile(
+              title: Text('版本更新', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              trailing: Icon(Icons.upgrade),
+              onTap: () {
+                // update here
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('用法', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              trailing: Icon(Icons.integration_instructions_outlined),
+              subtitle: Text(
+                  '\n\t1. 点击主页面右上角的齿轮配置动物价格、动物生长周期，保存返回主页面\n\n'
+                  '\t2. 在主页面填运营时间、动物初始状态，点击右下角按钮开始计算\n\n'
+                  '\t3. 在弹出的页面展示动物数量变化、收入变化，可以双值放大缩小和移动图面\n\n'),
+            ),
+            ListTile(
+              title: Text('联系我们', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              subtitle: Text('panghuachong@gmail.com'),
+              trailing: Icon(Icons.mail_outline_sharp),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
