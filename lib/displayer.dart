e0f9879088c76bb6bb793047da924384f71f0160
huachong.panghuach <huachong.panghuach@alibaba-inc.com>
2024-06-06 16:36:27 +0800

    no charts to run good on ios

--- lib/displayer.dart
+++ lib/displayer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+// import 'package:charts_flutter/flutter.dart' as charts;
 
 import 'farmModel.dart';
 
@@ -13,106 +13,106 @@ class StatisticPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("营收统计")),
       body: Flex(direction: Axis.vertical, children: [
-        Expanded(flex: 8, child: animalBar(clerk)),
-        Expanded(flex: 12, child: moneyBar(clerk)),
+        // Expanded(flex: 8, child: animalBar(clerk)),
+        // Expanded(flex: 12, child: moneyBar(clerk)),
       ]),
     );
   }
 
-  Widget moneyBar(CounterClerk clerk) {
-    List<MoneyStat> yearlyData = [], accumulateData = [];
-    for (var i = 1; i < clerk.annualIncrease.length; ++i) {
-      yearlyData.add(MoneyStat((2021 + i).toString(), clerk.annualIncrease[i]));
-      accumulateData
-          .add(MoneyStat((2021 + i).toString(), clerk.annualWealth[i]));
-    }
+  // Widget moneyBar(CounterClerk clerk) {
+  //   List<MoneyStat> yearlyData = [], accumulateData = [];
+  //   for (var i = 1; i < clerk.annualIncrease.length; ++i) {
+  //     yearlyData.add(MoneyStat((2021 + i).toString(), clerk.annualIncrease[i]));
+  //     accumulateData
+  //         .add(MoneyStat((2021 + i).toString(), clerk.annualWealth[i]));
+  //   }
 
-    var moneyDataSerial = [
-      charts.Series<MoneyStat, String>(
-        id: '年度利润',
-        colorFn: (_, __) => charts.MaterialPalette.purple.shadeDefault,
-        domainFn: (MoneyStat stat, _) => stat.year,
-        measureFn: (MoneyStat stat, _) => stat.money,
-        data: yearlyData,
-        labelAccessorFn: (MoneyStat stat, _) => '\$${stat.money.toString()}',
-        insideLabelStyleAccessorFn: (MoneyStat stat, _) =>
-            charts.TextStyleSpec(),
-      ),
-      charts.Series<MoneyStat, String>(
-        id: '累计利润',
-        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-        domainFn: (MoneyStat stat, _) => stat.year,
-        measureFn: (MoneyStat stat, _) => stat.money,
-        data: accumulateData,
-        labelAccessorFn: (MoneyStat stat, _) => '${stat.money.toString()}',
-        outsideLabelStyleAccessorFn: (MoneyStat stat, _) =>
-            charts.TextStyleSpec(),
-      ),
-    ];
+  //   var moneyDataSerial = [
+  //     charts.Series<MoneyStat, String>(
+  //       id: '年度利润',
+  //       colorFn: (_, __) => charts.MaterialPalette.purple.shadeDefault,
+  //       domainFn: (MoneyStat stat, _) => stat.year,
+  //       measureFn: (MoneyStat stat, _) => stat.money,
+  //       data: yearlyData,
+  //       labelAccessorFn: (MoneyStat stat, _) => '\$${stat.money.toString()}',
+  //       insideLabelStyleAccessorFn: (MoneyStat stat, _) =>
+  //           charts.TextStyleSpec(),
+  //     ),
+  //     charts.Series<MoneyStat, String>(
+  //       id: '累计利润',
+  //       colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
+  //       domainFn: (MoneyStat stat, _) => stat.year,
+  //       measureFn: (MoneyStat stat, _) => stat.money,
+  //       data: accumulateData,
+  //       labelAccessorFn: (MoneyStat stat, _) => '${stat.money.toString()}',
+  //       outsideLabelStyleAccessorFn: (MoneyStat stat, _) =>
+  //           charts.TextStyleSpec(),
+  //     ),
+  //   ];
 
-    return charts.BarChart(
-      moneyDataSerial,
-      animate: true,
-      barGroupingType: charts.BarGroupingType.stacked,
-      barRendererDecorator: charts.BarLabelDecorator<String>(
-          labelAnchor: charts.BarLabelAnchor.middle,
-          labelPadding: -50),
-      behaviors: [
-        charts.SlidingViewport(),
-        charts.PanAndZoomBehavior(),
-        charts.DomainHighlighter(),
-        charts.SeriesLegend()
-      ],
-      domainAxis:
-          charts.OrdinalAxisSpec(viewport: charts.OrdinalViewport('2020', 4)),
-      // vertical: false,
-    );
-  }
+  //   return charts.BarChart(
+  //     moneyDataSerial,
+  //     animate: true,
+  //     barGroupingType: charts.BarGroupingType.stacked,
+  //     barRendererDecorator: charts.BarLabelDecorator<String>(
+  //         labelAnchor: charts.BarLabelAnchor.middle,
+  //         labelPadding: -50),
+  //     behaviors: [
+  //       charts.SlidingViewport(),
+  //       charts.PanAndZoomBehavior(),
+  //       charts.DomainHighlighter(),
+  //       charts.SeriesLegend()
+  //     ],
+  //     domainAxis:
+  //         charts.OrdinalAxisSpec(viewport: charts.OrdinalViewport('2020', 4)),
+  //     // vertical: false,
+  //   );
+  // }
 
-  Widget animalBar(CounterClerk clerk) {
-    List<AnimalStat> cubData = [], femaleData = [];
-    for (var i = 1; i < clerk.annualCub.length; ++i) {
-      cubData.add(AnimalStat(i, clerk.annualCub[i]));
-      femaleData.add(AnimalStat(i, clerk.annualMaturedFemale[i]));
-    }
+  // Widget animalBar(CounterClerk clerk) {
+  //   List<AnimalStat> cubData = [], femaleData = [];
+  //   for (var i = 1; i < clerk.annualCub.length; ++i) {
+  //     cubData.add(AnimalStat(i, clerk.annualCub[i]));
+  //     femaleData.add(AnimalStat(i, clerk.annualMaturedFemale[i]));
+  //   }
 
-    var animalDataSerial = [
-      charts.Series<AnimalStat, int>(
-        id: '成年雌性数量',
-        colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
-        domainFn: (AnimalStat stat, _) => stat.year,
-        measureFn: (AnimalStat stat, _) => stat.count,
-        dashPatternFn: (_, __) => [8, 2, 4, 2],
-        data: cubData,
-      ),
-      charts.Series<AnimalStat, int>(
-        id: '幼崽数量',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (AnimalStat stat, _) => stat.year,
-        measureFn: (AnimalStat stat, _) => stat.count,
-        data: femaleData,
-      )
-    ];
+  //   var animalDataSerial = [
+  //     charts.Series<AnimalStat, int>(
+  //       id: '成年雌性数量',
+  //       colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
+  //       domainFn: (AnimalStat stat, _) => stat.year,
+  //       measureFn: (AnimalStat stat, _) => stat.count,
+  //       dashPatternFn: (_, __) => [8, 2, 4, 2],
+  //       data: cubData,
+  //     ),
+  //     charts.Series<AnimalStat, int>(
+  //       id: '幼崽数量',
+  //       colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+  //       domainFn: (AnimalStat stat, _) => stat.year,
+  //       measureFn: (AnimalStat stat, _) => stat.count,
+  //       data: femaleData,
+  //     )
+  //   ];
 
-    return charts.LineChart(
-      animalDataSerial,
-      animate: true,
-      defaultRenderer: charts.LineRendererConfig(
-          // 圆点大小
-          radiusPx: 5.0,
-          stacked: false,
-          // 线的宽度
-          strokeWidthPx: 2.0,
-          // 是否显示线
-          includeLine: true,
-          // 是否显示圆点
-          includePoints: true),
-      behaviors: [
-        charts.SeriesLegend(),
-        charts.PanAndZoomBehavior(),
-      ],
-    );
-  }
+  //   return charts.LineChart(
+  //     animalDataSerial,
+  //     animate: true,
+  //     defaultRenderer: charts.LineRendererConfig(
+  //         // 圆点大小
+  //         radiusPx: 5.0,
+  //         stacked: false,
+  //         // 线的宽度
+  //         strokeWidthPx: 2.0,
+  //         // 是否显示线
+  //         includeLine: true,
+  //         // 是否显示圆点
+  //         includePoints: true),
+  //     behaviors: [
+  //       charts.SeriesLegend(),
+  //       charts.PanAndZoomBehavior(),
+  //     ],
+  //   );
+  // }
 }
 
 class AnimalStat {
